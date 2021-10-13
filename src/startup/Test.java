126e0e326ef969e70469af2be33e644380569928
aks60 <gonved60@gmail.com>
2021-10-13 10:47:11 +0300

    Перед удалением mapJoin.

--- src/startup/Test.java
+++ src/startup/Test.java
@@ -81,7 +81,7 @@ public class Test {
 
         Query.connection = Test.connect2();
         builder.Wincalc iwin = new builder.Wincalc();
-        String _case = "one";
+        String _case = "max";
 
         if (_case.equals("one")) {
             iwin.build(builder.script.Winscript.test(601007, false));
