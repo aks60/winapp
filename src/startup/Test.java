071140529050db48ef6ca22a7fc1b1dd5408d9ec
aks <aks@aks-pk>
2023-01-31 19:29:45 +0300

    Начало реализации идей перенести наборы фуррн. в артикулы.

--- src/startup/Test.java
+++ src/startup/Test.java
@@ -67,7 +67,7 @@ public class Test {
         eProp.dev = true;
         try {
             //PSConvert.exec();
-            wincalc();
+            //wincalc();
             //param();
             //query();
             //frame();
